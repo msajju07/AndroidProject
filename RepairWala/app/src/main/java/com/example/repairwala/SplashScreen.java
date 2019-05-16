cdd684adbbac43546728839dd1c530e11d0c240c
Aroba Maheen <arobamaheen@gmail.com>
2019-05-16 18:44:50 +0500

    Update Splash

--- RepairWala/app/src/main/java/com/example/repairwala/SplashScreen.java
+++ RepairWala/app/src/main/java/com/example/repairwala/SplashScreen.java
@@ -1,13 +1,39 @@
 package com.example.repairwala;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
 
+    long Delay=4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
+
+        Timer RunSplash = new Timer();
+
+        // Task to do when the timer ends
+        TimerTask ShowSplash = new TimerTask() {
+            @Override
+            public void run() {
+                // Close SplashScreenActivity.class
+                finish();
+
+                // Start MainActivity.class
+                Intent myIntent = new Intent(SplashScreen.this,
+                        MainActivity.class);
+                startActivity(myIntent);
+            }
+        };
+
+        // Start the timer
+        RunSplash.schedule(ShowSplash, Delay);
     }
 }
