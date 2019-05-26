5d10ace2e8e3fa5c80acab0b45d4ce4ee1548c41
Aroba Maheen <arobamaheen@gmail.com>
2019-05-26 23:24:10 +0500

    service man activities

---
+++ RepairWala/app/src/main/java/com/example/repairwala/ServiceManCalendar.java
@@ -0,0 +1,13 @@
+package com.example.repairwala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceManCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_man_calendar);
+    }
+}
--- RepairWala/app/src/main/java/com/example/repairwala/Settings.java
+++ RepairWala/app/src/main/java/com/example/repairwala/Settings.java
@@ -1,7 +1,10 @@
 package com.example.repairwala;
 
+import android.content.Intent;
+import android.support.design.internal.NavigationMenu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void Back(View view) {
+        Intent intent = new Intent(this,navigation.class);
+        startActivity(intent);
     }
 }
